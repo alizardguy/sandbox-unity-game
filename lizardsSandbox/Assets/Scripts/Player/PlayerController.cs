9a8d43514488910c1b303bccf4cd20bf2e1e8959
alizardguy <42082829+alizardguy@users.noreply.github.com>
2022-07-03 16:32:38 -0700

    You can walk now
    Todo:
    Make moving and groovin feel good

--- lizardsSandbox/Assets/Scripts/Player/PlayerController.cs
+++ lizardsSandbox/Assets/Scripts/Player/PlayerController.cs
@@ -7,14 +7,18 @@ public class PlayerController : MonoBehaviour
     //Vars and fields
     [SerializeField] Transform playerCamera;
     [SerializeField] float mouseSensitivity = 3.5f;
+    [SerializeField] float walkSpeed = 6;
 
     [SerializeField] bool lockCursor = true;
 
     float cameraPitch = 0.0f;
+    CharacterController controller = null;
 
     // Start is called before the first frame update
     void Start()
     {
+        controller = GetComponent<CharacterController>(); //get character controller off the slot
+
         if (lockCursor)
         {
             Cursor.lockState = CursorLockMode.Locked;
@@ -25,7 +29,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        UpdateMouseLook(); //calls UpdateMouseLook   
+        UpdateMouseLook(); 
+        UpdateMovement();
     }
 
     // Called by update void
@@ -40,5 +45,16 @@ public class PlayerController : MonoBehaviour
         playerCamera.localEulerAngles = Vector3.right * cameraPitch;
 
         transform.Rotate(Vector3.up * mouseDelta.x * mouseSensitivity); //affects the object the script is on (the player root slot)
+    }
+
+    // Movment update void
+    void UpdateMovement()
+    {
+        Vector2 inputDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")); //input direction value (hopefully this is the modern binding system)
+        inputDir.Normalize();
+
+        Vector3 velocity = (transform.forward * inputDir.y + transform.right * inputDir.x) * walkSpeed;
+
+        controller.Move(velocity * Time.deltaTime); //apply movement
     }
 }
